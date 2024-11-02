ef4edd546ed2887fe334741a5eb8a65ef6dd8a0c
slashexx <dhruvpuri.35@gmail.com>
2024-11-02 22:03:23 +0530

    Frontend fin.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Make sure to import BrowserRouter
 import App from './App.tsx';
 import './index.css';
-import { IntroPage } from './IntroPage.tsx';
+import IntroPage  from './IntroPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
