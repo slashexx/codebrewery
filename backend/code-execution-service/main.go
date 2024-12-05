5a81083f1ca2f40988e871c6809c6a006e63ea0d
slashexx <dhruvpuri.35@gmail.com>
2024-12-05 19:06:59 +0530

    updating CES
    Signed-off-by: slashexx <dhruvpuri.35@gmail.com>

--- backend/code-execution-service/main.go
+++ backend/code-execution-service/main.go
@@ -120,7 +120,7 @@ func executeCode(language string, code string) (string, error) {
 }
 // https://codebrewery-api-gateway.onrender.com
 func enableCors(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "https://codebrewery-api-gateway.onrender.com")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
